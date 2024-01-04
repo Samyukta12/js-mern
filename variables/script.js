4215d09408ece047878cd80d38b4a688845c5c36
Sujata <sujatajoc123@gmail.com>
2024-01-04 18:13:20 +0545

    function

--- variables/script.js
+++ variables/script.js
@@ -4,3 +4,9 @@ let myName = "sujata";
 const age = 23;
 var surName = "joshi";
 console.log("My name is " + myName + " " + surName);
+
+const birthYear = Number(prompt("Enter your birth year?"));
+// console.log(birthYear);
+
+const currentAge = Number(2024 - birthYear);
+console.log(currentAge);
