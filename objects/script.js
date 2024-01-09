86e6688c471d7f97b485bad7421804311f8601ca
Sujata <sujatajoc123@gmail.com>
2024-01-09 09:05:00 +0545

    obj 2

--- objects/script.js
+++ objects/script.js
@@ -1,32 +1,43 @@
-//object // crud operation
+// //object // crud operation
 
-//create
-const myInfo = {
-  fName: "sujata",
-  lName: "joshi",
-  age: 23,
-  edu: "Bachelor",
-  doB: 2000,
-  isFemale: true,
+// //create
+// const myInfo = {
+//   fName: "sujata",
+//   lName: "joshi",
+//   age: 23,
+//   edu: "Bachelor",
+//   doB: 2000,
+//   isFemale: true,
 
-  // using function
-  // Note: we can use this. in es7 function but we have to use object name in arrow function because arrow function is local function.
+//   // using function
+//   // Note: we can use this. in es7 function but we have to use object name in arrow function because arrow function is local function.
 
-  //arrow function
-  getFullName: () => {
-    return myInfo.fName + " " + myInfo.edu;
+//   //arrow function
+//   getFullName: () => {
+//     return myInfo.fName + " " + myInfo.edu;
+//   },
+// };
+
+// //read
+// console.log(myInfo.getFullName());
+// console.log(myInfo.fName);
+
+// //update
+// myInfo.isFemale = false;
+// console.log(myInfo.isFemale);
+// // or console.log(myInfo.isFemale= false);
+
+// // delete
+// delete myInfo.age;
+// console.log(myInfo);
+
+const person = {
+  dob: 2000,
+  name: "sujata",
+
+  calcAge: function () {
+    let present = 2024 - this.dob;
+    return present;
   },
 };
-
-//read
-console.log(myInfo.getFullName());
-console.log(myInfo.fName);
-
-//update
-myInfo.isFemale = false;
-console.log(myInfo.isFemale);
-// or console.log(myInfo.isFemale= false);
-
-// delete
-delete myInfo.age;
-console.log(myInfo);
+console.log(person.calcAge());
